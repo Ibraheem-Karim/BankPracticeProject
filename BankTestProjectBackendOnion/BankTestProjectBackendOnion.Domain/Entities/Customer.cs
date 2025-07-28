6eb0b9e87ebfec8bd1f5005207442c4a83a912d0
Ebrahim Abdelkarim <ebrahimabdelkarim57@gmail.com>
2025-07-28 14:16:48 +0300

    last commit before migration

--- BankTestProjectBackendOnion/BankTestProjectBackendOnion.Domain/Entities/Customer.cs
+++ BankTestProjectBackendOnion/BankTestProjectBackendOnion.Domain/Entities/Customer.cs
@@ -12,16 +12,7 @@ namespace BankTestProjectBackendOnion.Domain.Entities
     {
         public string FullName { get; set; } = null!;
 
-
-        public string? Phone { get; set; }
-
         public DateTime CreatedAt { get; set; }
-
-        public string CustomerId { get; set; } = null!;
-
-
-
-        public virtual ICollection<Account> Accounts { get; set; } = new List<Account>();
 
 
     }
